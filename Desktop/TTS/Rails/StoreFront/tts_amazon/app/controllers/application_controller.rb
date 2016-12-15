95dc538c314817c13b73f6076baad9f76b83e403
Nathan Ooley <nate@appmosphereinc.com>
2016-12-15 09:10:04 -0500

    saving good working copy

--- Desktop/TTS/Rails/StoreFront/tts_amazon/app/controllers/application_controller.rb
+++ Desktop/TTS/Rails/StoreFront/tts_amazon/app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
+
+  before_filter :categories
+
+  def categories
+    @categories = Category.all
+  end
 
 
 
