78132fb639a1e78b78dbcab5e48a9842288018c4
Nathan Ooley <nate@appmosphereinc.com>
2016-12-15 09:54:27 -0500

    working code step 2

--- Desktop/TTS/Rails/StoreFront/tts_amazon/app/controllers/application_controller.rb
+++ Desktop/TTS/Rails/StoreFront/tts_amazon/app/controllers/application_controller.rb
@@ -4,13 +4,17 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  before_filter :categories
+  before_filter :categories, :brands
 
   def categories
     @categories = Category.all
   end
 
-
-
-
+  def brands
+    @brands = Product.pluck(:brand).
+    sort.uniq!
+    if @brands == nil
+      @brands = Product.pluck(:brand).sort
+    end
+  end
 end
